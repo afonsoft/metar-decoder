78c3914afdcef99bc7866e54c75aac04727b02ed
Afonso Dutra Nogueira Filho <adfilho@voegol.com.br>
2023-03-02 08:14:56 -0300

    Fix Sonar

--- sonarcloud.bat
+++ sonarcloud.bat
@@ -1,7 +1,7 @@
 @echo off
 REM dotnet tool install --global dotnet-sonarscanner
 dotnet restore MetarDecoder.sln --ignore-failed-sources
-dotnet sonar\SonarScanner.MSBuild.dll begin /o:"afonsoft-github" /k:"metar-decoder" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=a10c5ba8c51553dcdbe8f87d07806305bf6b8eeb /d:sonar.scm.provider=git /d:sonar.coverage.exclusions="**Test*.cs" /d:sonar.cs.vstest.reportsPaths=resultTest/*.trx
+dotnet sonar\SonarScanner.MSBuild.dll begin /o:"afonsoft-github" /k:"metar-decoder" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login={KEY} /d:sonar.scm.provider=git /d:sonar.coverage.exclusions=**Test*.cs,**/*Test*.cs /d:sonar.cs.vstest.reportsPaths=resultTest/*.trx
 dotnet build MetarDecoder.sln --configuration Release --verbosity minimal --no-incremental --ignore-failed-sources /nr:false
 echo.
 echo START Metar TEST
@@ -12,6 +12,6 @@ echo START Taf TEST
 echo.
 dotnet test tests/Taf.Decoder.Tests/Taf.Decoder.Tests.csproj --collect:"Code Coverage" --logger "trx;LogFileName=Taf.Decoder.Tests.trx" --results-directory resultTest/ --no-build --no-restore --configuration release -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=cobertura
 echo.
-dotnet sonar\SonarScanner.MSBuild.dll end /d:sonar.login=a10c5ba8c51553dcdbe8f87d07806305bf6b8eeb
+dotnet sonar\SonarScanner.MSBuild.dll end /d:sonar.login={KEY}
 echo .
 pause
