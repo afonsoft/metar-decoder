35126302ca4f3be12a8edbed7df8bd0f88887b51
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2024-03-27 12:00:25 -0300

    Docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,15 +14,16 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
 templates_path = ['_templates']
 exclude_patterns = []
+extensions = ['sphinx_csharp.csharp']
 
-
+from recommonmark.parser import CommonMarkParser
+source_parsers = {'.md': CommonMarkParser}
+source_suffix = ['.rst', '.md']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
